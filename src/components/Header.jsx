395cf41dcf925ffc08c4bb44d61ab19811a8f0ce
Adam Hughes <adamjordanhughes@gmail.com>
2023-04-19 15:57:13 -0400

    Add layout for the app

---
+++ src/components/Header.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Header = () => {
+
+
+    return (
+
+        <div className="navbar bg-base-100">
+            <a className="btn btn-ghost normal-case text-xl">NYT</a>
+        </div>
+
+    );
+}
+
+export default Header;
