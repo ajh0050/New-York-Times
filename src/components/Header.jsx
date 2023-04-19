db37c0c409bf516cfe6c717ec16f77f084d6400f
Adam Hughes <adamjordanhughes@gmail.com>
2023-04-19 16:24:58 -0400

    Create Landing component and fetching for articles

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,9 @@
 import React from 'react';
 
 const Header = () => {
-
-
     return (
 
-        <div className="navbar bg-base-100">
+        <div className="navbar bg-base-100 m-4">
             <a className="btn btn-ghost normal-case text-xl">NYT</a>
         </div>
 
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,8 @@
-
+import Landing from '../components/Home'
 export default function Home() {
   return (
     <>
-      <h1 className="text-center">Next.js + Google Fonts</h1>
+      <Landing />
     </>
   )
 }
