97855bfa7bf6b92cb76e60cba7d5ca9897a72389
Adam Hughes <adamjordanhughes@gmail.com>
2023-04-21 13:33:25 -0400

    Update import to chromium from aws and change browser to be headless and have aws args

--- src/pages/api/articleText.js
+++ src/pages/api/articleText.js
@@ -1,38 +1,56 @@
-import puppeteer from 'puppeteer';
+// import puppeteer from 'puppeteer';
+import chromium from 'chrome-aws-lambda';
+
 
 export default async function handler(req, res) {
-  // Check if the request method is POST and has a valid URL in the request body
-  if (req.method === 'POST' && req.body.url) {
-    try {
-      // Launch the browser
-      const browser = await puppeteer.launch();
-      // Create a new page
-      const page = await browser.newPage();
-      // Navigate to the desired URL
-      await page.goto(req.body.url);
+    // Check if the request method is POST and has a valid URL in the request body
+    if (req.method === 'POST' && req.body.url) {
+        try {
+            // Launch the browser
+            //   const browser = await puppeteer.launch();
+            // const browser = await puppeteer.launch({
+            //     headless: true,
+            //     args: [
+            //       '--no-sandbox',
+            //       '--disable-setuid-sandbox',
+            //       '--disable-dev-shm-usage',
+            //       '--disable-accelerated-2d-canvas',
+            //       '--disable-gpu'
+            //     ],
+            //   });
+            const browser = await chromium.puppeteer.launch({
+                args: chromium.args,
+                defaultViewport: chromium.defaultViewport,
+                executablePath: await chromium.executablePath,
+                headless: chromium.headless,
+            });
+            // Create a new page
+            const page = await browser.newPage();
+            // Navigate to the desired URL
+            await page.goto(req.body.url);
 
-      // Extract the headline and article text
-      const data = await page.evaluate(() => {
-        const articleParagraphs = Array.from(document.querySelectorAll('section[name="articleBody"] p')).map(
-          (paragraph) => paragraph.innerText
-        );
-        const articleText = articleParagraphs.join('\n');
+            // Extract the headline and article text
+            const data = await page.evaluate(() => {
+                const articleParagraphs = Array.from(document.querySelectorAll('section[name="articleBody"] p')).map(
+                    (paragraph) => paragraph.innerText
+                );
+                const articleText = articleParagraphs.join('\n');
 
-        return {
-          articleText,
-        };
-      });
+                return {
+                    articleText,
+                };
+            });
 
-      // Close the browser
-      await browser.close();
+            // Close the browser
+            await browser.close();
 
-      // Send the extracted data as a JSON response
-      res.status(200).json(data);
-    } catch (error) {
-      console.error('Error scraping article:', error);
-      res.status(500).json({ error: 'Error scraping article' });
+            // Send the extracted data as a JSON response
+            res.status(200).json(data);
+        } catch (error) {
+            console.error('Error scraping article:', error);
+            res.status(500).json({ error: 'Error scraping article' });
+        }
+    } else {
+        res.status(400).json({ error: 'Invalid request method or missing URL' });
     }
-  } else {
-    res.status(400).json({ error: 'Invalid request method or missing URL' });
-  }
 }
